f77ab1e27dcb56f5e89cc0ee9e75cb125181530c
PTH732 <phanhieu07032002@gmail.com>
2025-06-02 11:13:06 +0700

    ok

--- odoo/custom-addons/module_v_league/controllers/main.py
+++ odoo/custom-addons/module_v_league/controllers/main.py
@@ -7,10 +7,6 @@ from odoo.http import request
 class VLeagueController(http.Controller):
     @http.route('/vleague/players', type='http', auth='public', website=True)
     def show_players(self, **kwargs):
-        """
-        Hàm này sẽ hiển thị danh sách cầu thủ trên website.
-        Đường dẫn truy cập: http://localhost:8017/vleague/players
-        """
         players = request.env['football.player'].sudo().search([])
 
         return request.render('module_v_league.players_template', {
@@ -19,10 +15,6 @@ class VLeagueController(http.Controller):
 
     @http.route('/vleague/player/<int:player_id>', type='http', auth='public', website=True)
     def show_player_detail(self, player_id, **kwargs):
-        """
-        Xem chi tiết 1 cầu thủ dựa vào ID.
-        Ví dụ: http://localhost:8017/vleague/player/1
-        """
         player = request.env['football.player'].sudo().browse(player_id)
         if not player.exists():
             return request.not_found()
