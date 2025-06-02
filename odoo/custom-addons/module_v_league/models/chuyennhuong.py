5a3ad74f20fd9665246b07ec9c40c80f6cf959c8
PTH732 <phanhieu07032002@gmail.com>
2025-06-02 09:26:19 +0700

    ok

--- odoo/custom-addons/module_v_league/models/chuyennhuong.py
+++ odoo/custom-addons/module_v_league/models/chuyennhuong.py
@@ -9,3 +9,19 @@ class FootballTransfer(models.Model):
     to_club_id = fields.Many2one('football.club', string='To Club')
     transfer_date = fields.Date()
     transfer_fee = fields.Float()
+
+    @api.model
+    def create(self, vals):
+        transfer = super().create(vals)
+        # Cập nhật club_id của cầu thủ
+        if transfer.player_id and transfer.to_club_id:
+            transfer.player_id.club_id = transfer.to_club_id.id
+        return transfer
+
+    def write(self, vals):
+        res = super().write(vals)
+        for rec in self:
+            # Nếu có cập nhật to_club_id thì cập nhật club_id của cầu thủ
+            if 'to_club_id' in vals and rec.to_club_id:
+                rec.player_id.club_id = rec.to_club_id.id
+        return res
